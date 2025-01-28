ce2c6e312dc4f55836fbf0551f64a415edfded23
ramadhanharo <ramadhan.haro@moringaschool.com>
2025-01-28 14:11:50 +0300

    initial commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -8,7 +8,6 @@ import {
 } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { server } from "../mocks/server";
-
 import App from "../components/App";
 
 beforeAll(() => server.listen());
